497c3cbfb841340515de364de2e3cb9dc08007e6
Santos V <santhos2904@gmail.com>
2018-07-02 08:48:08 -0400

    Lecture 91

---
+++ Javascript/Lecture 91.js
@@ -0,0 +1,12 @@
+Null and Undefined
+//The two other primitives are null and undefined
+
+//Variables that are declared but not 
+//initialized are undefined
+//The following variables are undefined:
+var name;
+var age;
+
+//null is "explicitly nothing"
+var currentPlayer = "charlie";
+currentPlayer = null;   //game over
