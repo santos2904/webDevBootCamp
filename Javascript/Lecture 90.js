d00078756486ef78e119d37b14d737803910ec42
Santos V <santhos2904@gmail.com>
2018-07-01 21:20:31 -0400

    update

---
+++ Javascript/Lecture 90.js
@@ -0,0 +1,20 @@
+Variables
+//Variables are simply containers that store values
+//They follow this pattern:
+var yourVariableName = yourValue;
+
+//They can store all of the values we've seen
+var name = "Rusty";
+var secretNumber = 73;
+var isAdorable = true;
+
+//Recall the stored value by calling the variable name
+var name = "Rusty";
+"hello there " + name    //"hello there Rusty"
+
+var num = 37;
+num + 3 + 10    //50
+
+//We can also update existing variables
+var name = "Robert";
+name = "Bob";
