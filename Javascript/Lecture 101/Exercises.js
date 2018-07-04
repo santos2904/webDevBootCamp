a29590dafa56b127ac2802bcd06f8a189a57ad28
Santos V <santhos2904@gmail.com>
2018-07-03 21:50:27 -0400

    update

---
+++ Javascript/Lecture 101/Exercises.js
@@ -0,0 +1,41 @@
+Exercise 1
+
+var x = 10;
+var y = "a"
+
+y === "b" || x >= 10 /*false || true*/
+true
+
+
+
+Exercise 2
+
+var x = 3;
+var y = 8;
+
+!(x == "3" || x === y) && !(y != 8 && x <= y)/*false && true*/
+false
+
+
+
+Truthy and Falsy Values
+
+Exercise Try
+
+!"Hello World"
+false
+
+!""
+true
+
+!null
+true
+
+!0
+true
+
+!-1
+false
+
+!NaN
+true
