867bea7878cc4baeb14ab4377b023bd9fbfde95d
Santos V <santhos2904@gmail.com>
2018-07-05 19:59:29 -0400

    Lecture 107

--- Javascript/Lecture 107/whileLoops.js
+++ Javascript/Lecture 107/whileLoops.js
@@ -8,3 +8,29 @@ while (num <= 10) {
 }
 
 /*output: 1, 3, 5, 7, 9*/
+
+
+/*Exercise 2*/
+
+var num = 1
+
+while(num <= 20) {
+ if(num % 4 === 0){
+   console.log(num);
+ }
+ num++;
+}
+
+/*output: 4, 8, 12, 16, 20*/
+
+
+/*Exercise 3*/
+
+var num = 100;
+
+while(num < 150) {
+  console.log(num + 1); 
+  num--;
+}
+
+/*output: infinite loop*/
