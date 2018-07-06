4c52c061744890a586a73bc3a4389ef40e1ab2d1
Santos V <santhos2904@gmail.com>
2018-07-06 14:52:55 -0400

    forLoopExercise

---
+++ Javascript/Lecutre 113/forLoopsExercise.js
@@ -0,0 +1,16 @@
+/*Exercise 1*/
+
+for(var i = 0; i < 16; i+=8){
+  console.log(i);
+}
+/*output: 8*/
+
+
+/*Exercise 2*/
+
+var str = "ahceclwlxo";
+
+for(var i = 1; i < str.length; i+=2){
+  console.log(str[i]);
+}
+/*output: hello*/
