7e9908e02009e80b4fd965af690cb077c7da8834
Santos V <santhos2904@gmail.com>
2018-07-08 16:52:40 -0400

    update

---
+++ Javascript/Arrays/Lecture 129/Exercises.js
@@ -0,0 +1,18 @@
+// Exercise 1
+var numbers = [22, 67, 33, 96, 88];
+
+//What does the following line print?
+console.log(numbers[numbers.length])
+// undefined
+
+
+// Exercise 2
+var friendGroups = [
+  ["Harry", "Ron", "Hermione"],
+  ["Malfoy", "Crabbe", "Goyle"],
+  ["Mooney", "Wormtail", "Prongs"]
+];
+
+//What is the result of this line:
+console.log(friendGroups[2][0]); // Gets the 3rd [2] item in the array ["Mooney", "Wormtail", "Prongs"] and finds the 1st index [0] which is Mooney
+// Mooney 
