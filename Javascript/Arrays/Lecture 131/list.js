51f77cd3d9836ee4706194abc6700cda3df31e65
Santos V <santhos2904@gmail.com>
2018-07-09 20:31:51 -0400

    update

--- Javascript/Arrays/Lecture 131/list.js
+++ Javascript/Arrays/Lecture 131/list.js
@@ -3,20 +3,29 @@ window.setTimeout(function() {
   // put all the rest of your JS code from the lecture here
 var input = prompt("What would you like to do?");
 
-while (input !== "quit") {
+while (input !== "quit"){
 	//handle input
-	if(input === "list") {
-		console.log(todos);
+	if(input === "list"){
+		console.log("---------------");
+		todos.forEach(function(todo, index){
+			console.log(index + ": " + todo);
+		});
+		console.log("---------------");
 	} else if(input === "add") {
 		//ask for new todo
 		var addTodo = prompt("Enter new todo");
 		//add to todos array
 		todos.push(addTodo);
-	} else if(input === "removelast") {
+		console.log("Added Todo")
+	} else if(input === "delete"){
+		var index = prompt("Enter the index of todo to delete");
+		todos.splice(index,1);
+		console.log("Deleted Todo # " + index);
+	}
+/*	} else if(input === "removelast") {
 		var removeTodo = todos.pop();
 	} else if(input === "removefirst") {
-		var removeTodo = todos.shift();
-	}
+		var removeTodo = todos.shift();*/
 	//ask again for new input
 	input = prompt("What would you like to do?");
 }
