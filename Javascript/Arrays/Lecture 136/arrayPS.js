08dbe49d0d7d742b7d670bbad87714e4b21d5719
Santos V <santhos2904@gmail.com>
2018-07-09 22:01:28 -0400

    update

--- Javascript/Arrays/Lecture 136/arrayPS.js
+++ Javascript/Arrays/Lecture 136/arrayPS.js
@@ -1,8 +1,38 @@
 
-function printReverse(array){
-	for (var i = array.length -1; i >= 0; i--){
-		console.log(array[i]);
-	}
-}
 
-printReverse([1,2,3,4])
+
+//PS2
+function isUniform(array){
+	var arr = array[0];
+	for (var i = 0; i < array.length; i++) {
+		if(array[i] !== arr) {
+			return false;
+		}
+	}
+	return true;
+}
+isUniform([1,2,3,4])
+isUniform([1,1,1,1])
+
+
+//PS3
+function sumArray(array){
+	var sum = 0;
+	array.forEach(function(add) {
+		sum += add;
+	});
+	return sum;
+}
+sumArray([1,2,3,4])
+
+//PS4
+function maxArray(array){
+	var max = array[0];
+	array.forEach(function(index){
+		if(array[index] > index){
+			max = array[index];
+		}
+	});
+	return max;
+}
+maxArray([10,20,100,30])
