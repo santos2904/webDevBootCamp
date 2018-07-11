882bccb40bc63f8fed8cb4c40c7445479291bcd2
Santos V <santhos2904@gmail.com>
2018-07-11 19:44:30 -0400

    update

--- Javascript/Arrays/Lecture 136/arrayPS.js
+++ Javascript/Arrays/Lecture 136/arrayPS.js
@@ -1,21 +1,22 @@
 // PS1 
 function printReverse(array){
-	for(var i = array.length - 1; i >= 0; i--) {
-		console.log(array[i]);
+	for (var i = array.length - 1; i >= 0; i--){
+		console.log(array[i])
 	}
 }
-
+printReverse([1,2,3,4])
 
 //PS2
 function isUniform(array){
-	var arr = array[0];
-	for (var i = 0; i < array.length; i++) {
-		if(array[i] !== arr) {
+	var num = array[0];
+	for(var i = 0; i < array.length; i++){
+		if (array[i] !== num) {
 			return false;
 		}
 	}
 	return true;
 }
+
 isUniform([1,2,3,4])
 isUniform([1,1,1,1])
 
@@ -30,8 +31,27 @@ function sumArray(array){
 }
 sumArray([1,2,3,4])
 
+/*function sumArray(array){
+	var total = 0;
+	for (i = 0; i < array.length; i++){
+		total += array[i];
+	}
+	return total;
+}*/
+
 //PS4
 function maxArray(array){
+	var max = array[0];
+	for(var i = 0; i < array.length; i++){
+		if(array[i] > i){
+			max = array[i];
+		}
+	}
+	return max;
+}
+maxArray([10,20,100,30])
+
+/*function maxArray(array){
 	var max = array[0];
 	array.forEach(function(index){
 		if(array[index] > index){
@@ -39,5 +59,4 @@ function maxArray(array){
 		}
 	});
 	return max;
-}
-maxArray([10,20,100,30])
+}*/
