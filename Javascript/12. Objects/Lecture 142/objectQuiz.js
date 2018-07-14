adbfd8760cd0fd1fe68aa58c8ae0ae8df93be6d9
Santos V <santhos2904@gmail.com>
2018-07-13 21:56:41 -0400

    update

---
+++ Javascript/12. Objects/Lecture 142/objectQuiz.js
@@ -0,0 +1,12 @@
+var someObject = {};
+
+//which of the following are valid:
+
+someObject._name = "Hedwig";
+
+someObject.age = 6;
+
+var prop = "color"
+someObject[prop] = "red";
+
+someObject.123 = true;
