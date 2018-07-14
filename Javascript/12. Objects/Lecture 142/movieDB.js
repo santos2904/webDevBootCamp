25df309790cf27a8750509f7189692c58a53b0bc
Santos V <santhos2904@gmail.com>
2018-07-13 22:23:19 -0400

    update

---
+++ Javascript/12. Objects/Lecture 142/movieDB.js
@@ -0,0 +1,39 @@
+/*MovieDB*/
+//Create an array of movie objects.
+//Each movie should have a title, rating and hasWatched properties.
+// Iterate through the array and print out something that looks like:
+
+var movieDB = [
+	{
+		title: "In Burges", 
+		hasWatched: false, 
+		rating: 5
+	},
+	{
+		title: "Frozen", 
+		hasWatched: true, 
+		rating: 4.5
+	},
+	{
+		title: "Mad Max Fury Road", 
+		hasWatched: true, 
+		rating: 5
+	},
+	{
+		title: "Les Miserables", 
+		hasWatched: false, 
+		rating: 3.5
+	},
+]
+
+movieDB.forEach(function(movie){
+	var result = "You have ";
+	if (movie.hasWatched){
+		result += "watched ";
+	} else {
+		result += "not seen ";
+	}
+	result += "\"" + movie.title + "\" - ";
+	result += movie.rating + " starts"
+	console.log(result)
+})
