b267ce004d57be7e9c233a01079be3dda7dab2bc
Santos V <santhos2904@gmail.com>
2018-07-15 09:40:51 -0400

    update

---
+++ Javascript/12. Objects/Lecture 144/jsMethods.js
@@ -0,0 +1,15 @@
+console.log("Connection established!")
+
+var obj = {
+	name: "Santos",
+	age: 29,
+	isAwakened: false,
+	friends: ["Kaja", "Sugi", "Pakki"],
+	birthYr: function(y,a){
+		return y - a;
+	}
+}
+// obj.birthYr(2018,29)
+// 1989
+
+
