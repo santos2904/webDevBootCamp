c6b310a1b7e5e1d890af69f68d638b5ba3de6880
Santos V <santhos2904@gmail.com>
2018-07-18 19:24:37 -0400

    update

--- DOM Manipulation/Lecture 151/selectors.js
+++ DOM Manipulation/Lecture 151/selectors.js
@@ -1,7 +1,8 @@
 console.log("Connection established...")
 
 //getElementById
-
 var tag = document.getElementById("highlight");
 
-//
+
+//getElementsByClassName
+var tags = document.getElementsByClassName("bolded");
