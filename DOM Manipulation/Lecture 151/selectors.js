b7833f74b88537c479b69ade0f4a473f16d14ba3
Santos V <santhos2904@gmail.com>
2018-07-18 20:41:40 -0400

    update

--- DOM Manipulation/Lecture 151/selectors.js
+++ DOM Manipulation/Lecture 151/selectors.js
@@ -6,3 +6,19 @@ var tag = document.getElementById("highlight");
 
 //getElementsByClassName
 var tags = document.getElementsByClassName("bolded");
+
+
+//getElementsByTagName
+//var tags = document.getElementsByTagName("h1");
+var idTags = document.getElementsByTagName("li");
+console.log(idTags[0]);
+
+
+//querySelector
+//select by ID
+//Will only return the first match
+var highlight = document.querySelector("#highlight");
+var bolded = document.querySelector(".bolded"); 
+var h1 = document.querySelector("h1"); 
+
+
