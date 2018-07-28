8bb6d5f7d97addf33714562268aa0ca8a51fdf30
Santos V <santhos2904@gmail.com>
2018-07-27 21:59:38 -0400

    update

--- Advanced DOM Manipulation/Lecture 159/listener.js
+++ Advanced DOM Manipulation/Lecture 159/listener.js
@@ -5,3 +5,14 @@ h1.addEventListener("click", function(){
 	h1.style.color = "red";
 	h1.textContent = "H1 WAS CLICKED!"
 })
+
+document.querySelector("ul").addEventListener("click", function(){
+	console.log("You clicked UL");
+})
+
+var lis = document.querySelectorAll("li");
+for (var i = 0; i < lis.length; i++){
+	lis[i].addEventListener("click", function(){
+		this.style.color = "pink";
+	})
+}
