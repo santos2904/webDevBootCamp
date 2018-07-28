4b55bd9e67cc625e6f410487834364aefb89c879
Santos V <santhos2904@gmail.com>
2018-07-28 11:54:41 -0400

    update

---
+++ Advanced DOM Manipulation/Lecture 159/listener.js
@@ -0,0 +1,32 @@
+var h1 = document.querySelector("h1");
+
+// changes h1 color to red when clicked
+h1.addEventListener("click", function(){
+	h1.style.color = "red";
+	h1.textContent = "H1 WAS CLICKED!"
+})
+
+document.querySelector("ul").addEventListener("click", function(){
+	console.log("You clicked UL");
+})
+
+// loops through each Li and console logs message and changes color
+var lis = document.querySelectorAll("li");
+for (var i = 0; i < lis.length; i++){
+	lis[i].addEventListener("click", function(){
+		this.style.color = "pink";
+		console.log("Li was clicked");
+	})
+}
+
+// You can also call back a function in event listner
+var b = document.querySelector("button");
+var p = document.querySelector("#para"); //id
+
+function clicked(){
+	p.textContent = "YOU DARE CLICK ME?!";
+	p.style.color = "red";
+	p.style.fontSize = "200%"
+}
+
+b.addEventListener("click", clicked)
