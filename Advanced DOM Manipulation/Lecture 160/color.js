64ace0b8092a3d777fdc94379823a16b767fd8d9
Santos V <santhos2904@gmail.com>
2018-07-29 15:57:16 -0400

    update

--- Advanced DOM Manipulation/Lecture 160/color.js
+++ Advanced DOM Manipulation/Lecture 160/color.js
@@ -1,3 +1,19 @@
-document.querySelector("button").addEventListener("click", function(){
-	document.querySelector("body").style.background = "purple";
+var button = document.querySelector("button");
+var isPurple = false;
+
+button.addEventListener("click", function(){
+	if(isPurple){
+		document.body.style.background = "purple";
+		isPurple = false;
+	} else {
+		document.body.style.background = "white";
+		isPurple = true;
+	}	
+	//isPurple = !isPurple;
 });
+
+
+//shorter code using css class to toggle the color
+/*button.addEventListener("click", function(){
+		document.body.classList.toggle("purple");
+});*/
