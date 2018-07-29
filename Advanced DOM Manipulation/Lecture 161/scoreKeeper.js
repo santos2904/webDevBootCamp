7656c98dad990115a915400eff968b80327b2e58
Santos V <santhos2904@gmail.com>
2018-07-29 17:12:47 -0400

    update

--- Advanced DOM Manipulation/Lecture 161/scoreKeeper.js
+++ Advanced DOM Manipulation/Lecture 161/scoreKeeper.js
@@ -1,7 +1,8 @@
-console.log("CONNECTION ESTABLISHED")
-
 var p1Button = document.querySelector("#p1Button");
 var p2Button = document.querySelector("#p2Button");
+var p1 = document.querySelector("#p1");
+var p2 = document.querySelector("#p2");
+var reset = document.querySelector("#reset");
 var p1Score = 0;
 var p2Score = 0;
 var winningScore = 5;
@@ -11,18 +12,30 @@ p1Button.addEventListener("click", function(){
 	if(!gameOver){
 		p1Score++;
 		if(p1Score === winningScore){
+			p1.classList.add("winner");
 			gameOver = true;
 		}
-		document.querySelector("#p1").textContent = p1Score;
+		p1.textContent = p1Score;
 	}
-})
+});
 
 p2Button.addEventListener("click", function(){
 	if(!gameOver){
 		p2Score++;
 		if(p2Score === winningScore){
+			p2.classList.add("winner");
 			gameOver = true;
 		}
-		document.querySelector("#p2").textContent = p2Score;
+		p2.textContent = p2Score;
 	}
-})
+});
+
+reset.addEventListener("click", function(){
+	p1Score = 0;
+	p2Score = 0;
+	p1.textContent = 0;
+	p2.textContent = 0;
+	p1.classList.remove("winner");
+	p2.classList.remove("winner");
+	gameOver = false;
+});
