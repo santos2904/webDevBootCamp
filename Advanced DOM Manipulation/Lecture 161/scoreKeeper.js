a2bbc6f9cc5c4017578dbda425dae8ebe20d5a06
Santos V <santhos2904@gmail.com>
2018-07-30 20:56:54 -0400

    update

--- Advanced DOM Manipulation/Lecture 161/scoreKeeper.js
+++ Advanced DOM Manipulation/Lecture 161/scoreKeeper.js
@@ -1,8 +1,10 @@
 var p1Button = document.querySelector("#p1Button");
 var p2Button = document.querySelector("#p2Button");
+var resetButton = document.querySelector("#reset");
 var p1 = document.querySelector("#p1");
 var p2 = document.querySelector("#p2");
-var reset = document.querySelector("#reset");
+var numInput = document.querySelector("input");
+var winDisplay = document.querySelector("p span");
 var p1Score = 0;
 var p2Score = 0;
 var winningScore = 5;
@@ -30,7 +32,11 @@ p2Button.addEventListener("click", function(){
 	}
 });
 
-reset.addEventListener("click", function(){
+resetButton.addEventListener("click", function(){
+	reset();
+});
+
+function reset(){
 	p1Score = 0;
 	p2Score = 0;
 	p1.textContent = 0;
@@ -38,4 +44,8 @@ reset.addEventListener("click", function(){
 	p1.classList.remove("winner");
 	p2.classList.remove("winner");
 	gameOver = false;
-});
+}
+
+numInput.addEventListener("change", function(){
+	winDisplay.textContent = numInput.value;
+})
