dd2176213e29cada5468d160a4f5bb202e50be04
Santos V <santhos2904@gmail.com>
2018-08-18 19:31:46 -0400

    update

--- jQuery/Lecture 183/methods.js
+++ jQuery/Lecture 183/methods.js
@@ -1,1 +1,11 @@
+$("h1").text();
+//"jQuery Methods Demo Page"
+
+$("ul").text();
+//"	Skittles M&Ms KitKat "
+
+$("li").text();
+//"SkittlesM&MsKitKat"
+
 $("ul").html();
+// " <li>Skittles</li> <li>M&amp;Ms</li> <li>KitKat</li>"
