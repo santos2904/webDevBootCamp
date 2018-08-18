12f373239d99368477841f2034541d68b3f9c57c
Santos V <santhos2904@gmail.com>
2018-08-17 21:50:50 -0400

    update

---
+++ jQuery/Lecture 181/selecting_jQuery.js
@@ -0,0 +1,17 @@
+//Selecting with jQuery
+
+$("SelectorGoesHere")
+
+//we select then manipulate
+
+//to select all img tags
+$("img")
+
+//to select all elements with class 'sale
+$(".sale")
+
+//to select element with id "bonus"
+$("#bonus")
+
+//to select all a tags inside of li's
+$("li a")
