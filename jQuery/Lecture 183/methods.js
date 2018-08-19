268594fa0b7d46e1f937c1473c364909d6be10f5
Santos V <santhos2904@gmail.com>
2018-08-18 21:07:03 -0400

    update

--- jQuery/Lecture 183/methods.js
+++ jQuery/Lecture 183/methods.js
@@ -10,9 +10,26 @@ $("li").text();
 $("ul").html();
 // " <li>Skittles</li> <li>M&amp;Ms</li> <li>KitKat</li>"
 
-$("li:last").html("<a href='www.google.ca'>click me</a>")
+$("li:last").html("<a href='https://en.wikipedia.org/wiki/Android_Pie'>P</a>")
 // redirects to google
 
-$("img").css("width", "200px");
+$("img").css({
+	width: "200px",
+	height: "200px"
+});
 
-$("img").attr("src", "https://i.imgflip.com/1tkfx0.jpg");
+$("img:first-of-type").attr("src", "https://i.imgflip.com/1tkfx0.jpg");
+$("img:last-of-type").attr("src", "https://i.pinimg.com/originals/d0/ce/f6/d0cef6569231e4cb71e4aa86bef250e5.jpg");
+
+$("input").val("Santos Phone");
+
+$("select").val();
+
+$("h1").addClass("correct");
+
+$("li:first-of-type").addClass("wrong");
+$("li:last-of-type").addClass("wrong");
+$("li:last-of-type").removeClass("wrong");
+
+$("li:last").toggleClass("done");
+
