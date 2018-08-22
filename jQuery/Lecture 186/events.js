ee0c58dfcdc0cdfc2f0fafd5a52e0996597475f5
Santos V <santhos2904@gmail.com>
2018-08-21 21:05:52 -0400

    update

--- jQuery/Lecture 186/events.js
+++ jQuery/Lecture 186/events.js
@@ -1,9 +1,29 @@
-$("h1").click(function(){
-	alert("H1 was clicked");
-});
-
+// click()
 $("button").click(function(){
 	var text = $(this).text();
 	console.log("You clicked " + text);
 	$(this).css("background", "green");
+});
+
+//keypress()
+$("input").keypress(function(event){
+	if(event.which === 13)
+	console.log("You pressed Enter");
+});
+
+//on()
+$("h1").on("click", function(){
+	$(this).css("color", "red")
+});
+
+$("input").on("keypress", function(){
+	console.log("Keypressed");
+});
+
+$("button").on("mouseenter", function(){
+	$(this).css("font-weight", "bold");
+});
+
+$("button").on("mouseleave", function(){
+	$(this).css("font-weight", "normal");
 });
