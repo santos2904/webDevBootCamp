fdbb777f33fa2bcd98d25f2de6885fc14a2a62df
Santos V <santhos2904@gmail.com>
2018-08-22 21:06:39 -0400

    update

--- Advanced jQuery/Lecture 190/effects.js
+++ Advanced jQuery/Lecture 190/effects.js
@@ -1,11 +1,25 @@
-$("button").on("click", function(){
-	$("div:first").fadeOut(1500);
+$("#button1").on("click", function(){
+	$("div").css("background", "red");
+	$("div:first").fadeOut(1500).text("Please don't fade me!");
+	$("div:nth(1)").fadeOut(3000).text("I'm begging you, please");;
+	$("div:last").fadeOut(5000, function(){
+		alert("Fadeout completed!");
+		$("#button2").css("display", "block");
+	}).text("Help help help!!");
+	$(this).css("display", "none");
 });
 
-$("button").on("click", function(){
-	$("div:nth(1)").fadeOut(3000);
+$("#button2").click(function(){
+	$("div").css("background", "green");
+	$("div").slideDown(3000, function(){
+		console.log("Revived!");
+		$("#button1").css("display", "block");
+	});
+	$(this).css("display", "none");
+	$("div:first").text("Yay, we've been revived");
+	$("div:nth(1)").text("Reborn!");
+	$("div:last").text("Thank you so much!");
 });
 
-$("button").click(function(){
-	$("div:last").fadeOut(6000);
-})
+//fadeToggle() can be used to fadeIn or fadeOut an element
+//slideToggle() can be used to slideUp and slideDown
