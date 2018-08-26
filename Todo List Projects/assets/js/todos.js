75d99601de7550c880404ca9a2bad912570493cc
Santos V <santhos2904@gmail.com>
2018-08-26 11:42:07 -0400

    update

--- Todo List Projects/assets/js/todos.js
+++ Todo List Projects/assets/js/todos.js
@@ -2,8 +2,9 @@ console.log("connection established")
 
 // check off todos by Clicking
 $("li").click(function(){
-	$(this).css({
-		color: "gray",
-		textDecoration: "line-through"
-	});
+	$(this).toggleClass("done")
 });
+
+$("span").click(function(){
+		$(this).parent().remove();
+	});
