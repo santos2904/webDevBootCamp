d67ff976ae0e0569bcc871f92c58a7403ce8165d
Santos V <santhos2904@gmail.com>
2018-08-26 15:32:59 -0400

    update

--- Todo List Projects/assets/js/todos.js
+++ Todo List Projects/assets/js/todos.js
@@ -1,10 +1,26 @@
 console.log("connection established")
 
-// check off todos by Clicking
-$("li").click(function(){
+//Check off todos by Clicking
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("done")
 });
 
-$("span").click(function(){
-		$(this).parent().remove();
+
+//Click on X to delete Todo
+$("ul").on("click", "span", function(event){
+		$(this).parent().fadeOut(500,function(){
+			$(this).remove();
+		});
+		event.stopPropagation();
 	});
+
+//Enter todos in the input and add it to Todo list
+$("input[type='text']").keypress(function(e){
+	if(e.which === 13){
+		//grab new todo text from input
+		var todo = $(this).val();
+		$(this).val("");
+		//create a new li and add it to ul
+		$("ul").append("<li><span>X</span> " + todo + "</li>");
+	}
+});
