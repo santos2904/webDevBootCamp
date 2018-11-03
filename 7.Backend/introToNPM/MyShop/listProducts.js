5d5387ba663368b6e6b5f7c5ca388e0dfbfeb851
$antos <santhos2904@gmail.com>
2018-11-03 18:16:22 -0400

    Update 7.Backend/introToNPM/MyShop/listProducts.js

--- 7.Backend/introToNPM/MyShop/listProducts.js
+++ 7.Backend/introToNPM/MyShop/listProducts.js
@@ -8,3 +8,11 @@ for(var i = 0; i < 10; i++){
     console.log(faker.commerce.productName() + " - $" + faker.commerce.price());    
 }
 
+
+// examples:
+// var randomName = faker.name.findName(); // Rowan Nikolaus
+// var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+// var randomCard = faker.helpers.createCard(); // random contact card containing many properties
+
+// console.log(randomName);
+// console.log(randomEmail);
