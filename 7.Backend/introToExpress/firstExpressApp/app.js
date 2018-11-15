4a6aa1a97aff197565097e365f4016cf6c7ad2d5
$antos <santhos2904@gmail.com>
2018-11-15 18:49:23 -0500

    Update 7.Backend/introToExpress/firstExpressApp/app.js

--- 7.Backend/introToExpress/firstExpressApp/app.js
+++ 7.Backend/introToExpress/firstExpressApp/app.js
@@ -1,15 +1,20 @@
 var express = require("express");
+
 var app = express();
 
 app.get("/", function(req, res){
-    res.send("hallo wold");
+    res.send("Scarborough uplink successful. Welcome Back Commander.");
 });
 
-// "/bye" => "Goodbye!"
-app.get("/bye", function(req, res){
-    res.send("Goodbye!");
+app.get("/execute", function(req, res){
+    res.send("Exeggutor.png");
 });
 
+app.get("/shutdown", function(req, res){
+    res.send("Initiating Shutdown sequence...");
+});
+
+app.get("")
 app.listen(8000, process.env.IP , function(){
     console.log("Server has started");
 });
