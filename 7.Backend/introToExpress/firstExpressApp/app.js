99e652b6e69f27a738b4d4f64275b88ad583439d
$antos <santhos2904@gmail.com>
2018-11-19 20:16:16 -0500

    Update app.js

--- 7.Backend/introToExpress/firstExpressApp/app.js
+++ 7.Backend/introToExpress/firstExpressApp/app.js
@@ -14,9 +14,25 @@ app.get("/shutdown", function(req, res){
     res.send("Initiating Shutdown sequence...");
 });
 
+app.get("/r/:subredditName", function(req, res){
+    res.send("Welcome to this subreddit");
+});
+
+app.get("/r/:subredditName/comments/:id/:title", function(req, res){
+    res.send("Welcome to the comment section");
+});
+
+app.get("*", function(req,res){
+    res.send("404 not found!");
+})
+
 app.get("")
 app.listen(8000, process.env.IP , function(){
     console.log("Server has started");
+    console.log("On the browser, go to:");
+    console.log("http://localhost:8000/");
+    console.log("http://localhost:8000/execute");
+    console.log("http://localhost:8000/shutdown");
 });
 
 
