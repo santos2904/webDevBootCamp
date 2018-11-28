def4bc1c710131c0ab1b2e29e5878a8ac0ee8ca2
$antos <santhos2904@gmail.com>
2018-11-27 20:10:08 -0500

    update

--- 7.Backend/moreExpress/EJSDemo/app.js
+++ 7.Backend/moreExpress/EJSDemo/app.js
@@ -2,7 +2,12 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res){
-    res.send("<h1>Welcome to the homepage!</h1><h2>This is a H2</h2>");
+    res.render("home.ejs");
+});
+
+app.get("/fallinlovewith/:thing", function(req, res){
+    var thing = req.params.thing;
+    res.render("love.ejs", {thingVar: thing});
 });
 
 app.listen(8000, process.env.IP , function(){
