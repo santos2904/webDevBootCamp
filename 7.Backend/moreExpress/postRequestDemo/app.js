a20b136007f829dc758ba1c56312eefebd365a02
$antos <santhos2904@gmail.com>
2018-12-06 21:26:07 -0500

    update

---
+++ 7.Backend/moreExpress/postRequestDemo/app.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+
+app.set("view eingine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.listen(8000, process.env.IP, function(){
+    console.log("Listening for Post Request");
+});
