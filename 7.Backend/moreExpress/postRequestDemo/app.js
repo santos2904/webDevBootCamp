5e1fc211df57e0ad01b4988618ad1ee3af271d33
$antos <santhos2904@gmail.com>
2018-12-07 20:51:04 -0500

    update

--- 7.Backend/moreExpress/postRequestDemo/app.js
+++ 7.Backend/moreExpress/postRequestDemo/app.js
@@ -7,6 +7,15 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
+app.get("/friends", function(req,res){
+    var friends = ["Kaja", "James", "Jessi", "Rajeev", "Emma", "Suki", "Sangi"]
+    res.render("friends", {friends: friends});
+});
+
+app.post("/addFriend", function(req, res){
+    res.send("You have reached the post route!");
+});
+
 app.listen(8000, process.env.IP, function(){
     console.log("Listening for Post Request");
 });
